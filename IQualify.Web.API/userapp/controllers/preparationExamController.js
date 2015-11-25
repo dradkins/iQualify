1cb1a033693b98f17a361010f1ded05ed7c55349
dradkins <dradkins11@gmail.com>
2015-11-25 15:58:11 +0500

    changes

--- IQualify.Web.API/userapp/controllers/preparationExamController.js
+++ IQualify.Web.API/userapp/controllers/preparationExamController.js
@@ -1,22 +1,29 @@
 ﻿(function (module) {
 
-    var preparationExamController = function ($scope, toastr, question) {
+    var preparationExamController = function ($scope, toastr, question, answer) {
 
-        $scope.totalQuestions = 10;
+        $scope.noOfTotalQuestions = 10;
         $scope.currentQuestion = {};
+        $scope.currentQuestionOptions = [];
+        $scope.isExamStart = false;
+        $scope.isExamComplete = false;
+        $scope.answer = {};
+        $scope.selectedAnswer = null;
         $scope.currentQuestionNumber = 0;
+        $scope.nextButtonEnabled = true;
+
         $scope.showExam = false;
         $scope.showStatus = false;
         $scope.selectedAnswer = null;
 
         $scope.getQuestions = function () {
-            if ($scope.totalQuestions > 50) {
+            if ($scope.noOfTotalQuestions > 50) {
                 toastr.warning("Maximum 50 questions allowed")
             }
-            if ($scope.totalQuestions < 10) {
+            if ($scope.noOfTotalQuestions < 10) {
                 toastr.warning("Minimum 10 questions allowed")
             }
-            question.getRandomQuestions($scope.totalQuestions).then(onQuestions, onQuestionsError);
+            question.getRandomQuestions($scope.noOfTotalQuestions).then(onQuestions, onQuestionsError);
         }
 
         $scope.nextQuestion = function () {
@@ -27,7 +34,9 @@
         }
 
         $scope.checkAnswer = function () {
-
+            $scope.nextButtonEnabled = false;
+            
+            answer.checkQuestionAnswer({questionId:$scope.currentQuestion.id, selectedAnswer:$scope.selectedAnswer}).then(onAnswer, onAnswerError);
         }
 
         $scope.finishExam = function () {
@@ -44,6 +53,32 @@
         var onQuestionsError = function (error) {
             console.log(error);
             toastr.error("Unable to load questions at this time. Please try later.")
+        }
+
+        var onAnswer = function (data) {
+            $scope.answer = data;
+            if ($scope.answer.isCorrect) {
+                proceedeToNextQuestion();
+            }
+        }
+
+        var proceedeToNextQuestion = function () {
+            $scope.selectedAnswer = null;
+            $scope.answer = {};
+            $scope.currentQuestionNumber++;
+            $scope.currentQuestion = $scope.questions[$scope.currentQuestionNumber];
+            fillOptions();
+        }
+
+        var onAnswerError = function (error) {
+            console.log(error);
+        }
+
+        var fillOptions = function () {
+            for (var i = 0; i < $scope.currentQuestion.totalNoOfOptions; i++) {
+                var chr = String.fromCharCode(65 + n);
+                $scope.currentQuestionOptions.push(chr);
+            }
         }
     }
 
